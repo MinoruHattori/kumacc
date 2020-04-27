a4423e6bea72ae70c9ec0a471f67f8ddbdad355c
kazukiigeta <igetakazuki@gmail.com>
2020-04-26 23:11:35 -0400

    add a tokenizer to allow space characters

--- main.c
+++ main.c
@@ -1,34 +1,123 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include <string.h>
+
+typedef enum {
+  TK_RESERVED,
+  TK_NUM,
+  TK_EOF,
+} TokenKind;
+
+typedef struct Token Token;
+struct Token {
+  TokenKind kind;
+  Token *next;
+  long val;
+  char *loc;
+  int len;
+};
+
+static void error(const char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  exit(1);
+}
+
+static bool equal(const Token *tok, const char *s) {
+  return strlen(s) == tok->len &&
+         !strncmp(tok->loc, s, tok->len);
+}
+
+static Token *skip(const Token *tok, const char *s) {
+  if (!equal(tok, s)) {
+    error("expected '%s'", s);
+  }
+  return tok->next;
+}
+
+static long get_number(const Token *tok) {
+  if (tok->kind != TK_NUM) {
+    error("expected a number");
+  }
+  return tok->val;
+}
+
+static Token *new_token(TokenKind kind, char *str, int len) {
+  Token *tok = calloc(1, sizeof(Token));
+  tok->kind = kind;
+  tok->loc = str;
+  tok->len = len;
+  return tok;
+}
+
+static Token *tokenize(char *p) {
+  Token head = {};
+  Token *cur = &head;
+
+  while (*p) {
+    if (isspace(*p)) {
+      p++;
+      continue;
+    }
+
+    if (*p == '+' || *p == '-') {
+      Token *nxt;
+      nxt = new_token(TK_RESERVED, p++, sizeof(char));
+      cur->next = nxt;
+      cur = nxt;
+      continue;
+    }
+
+    if (isdigit(*p)) {
+      Token *nxt;
+      nxt = new_token(TK_NUM, p, 0);
+      cur->next = nxt;
+      cur = nxt;
+      char *q = p;
+      cur->val = strtoul(p, &p, 10);
+      cur->len = p - q;
+      continue;
+    }
+
+    error("invalid token");
+  }
+
+  Token *nxt;
+  nxt = new_token(TK_EOF, p, 0);
+  cur->next = nxt;
+  cur = nxt;
+  return head.next;
+}
 
 int main(int argc, char **argv) {
   if (argc != 2) {
-    fprintf(stderr, "%s: invalid number of arguments\n", argv[0]);
-    return 1;
+    error("%s: invalid number of arguments\n", argv[0]);
   }
 
-  char *p = argv[1];
+  Token *tok = tokenize(argv[1]);
 
   printf(".intel_syntax noprefix\n");
   printf(".global main\n");
   printf("main:\n");
-  printf("  mov rax, %ld\n", strtol(p, &p, 10));
 
-  while (*p) {
-    if (*p == '+') {
-      p++;
-      printf("  add rax, %ld\n", strtol(p, &p, 10));
+  printf("  mov rax, %ld\n", get_number(tok));
+  tok = tok->next;
+
+  while (tok->kind != TK_EOF) {
+    if (equal(tok, "+")) {
+      printf("  add rax, %ld\n", get_number(tok->next));
+      tok = tok->next->next;
       continue;
     }
 
-    if (*p == '-') {
-      p++;
-      printf("  sub rax, %ld\n", strtol(p, &p, 10));
-      continue;
-    }
-
-    fprintf(stderr, "unexpected character: '%c'\n", *p);
-    return 1;
+    tok = skip(tok, "-");
+    printf("    sub rax, %ld\n", get_number(tok));
+    tok = tok->next;
   }
 
   printf("  ret\n");
--- test.sh
+++ test.sh
@@ -19,5 +19,6 @@ assert() {
 assert 0 0
 assert 42 42
 assert 32 '9+25-2'
+assert 96 ' 44 + 58 - 6'
 
 echo OK
