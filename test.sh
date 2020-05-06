68e68f0aa0f4dd932883138a69073f264ab94e76
kazukiigeta <igetakazuki@gmail.com>
2020-05-06 04:00:49 -0400

    Add ==, !=, <= and >= operators

--- test.sh
+++ test.sh
@@ -28,4 +28,23 @@ assert 30 '-14+44'
 assert 20 '- -20'
 assert 20 '- - +20'
 
+assert 0 '-1==0'
+assert 1 '33==32+1'
+assert 1 '3!=2'
+assert 0 '23-1!=22'
+
+assert 1 '0<2'
+assert 0 '3<3'
+assert 0 '2<0'
+assert 1 '-1<=0'
+assert 1 '2<=2'
+assert 0 '3<=2'
+
+assert 1 '2>0'
+assert 0 '3>3'
+assert 0 '2>3'
+assert 1 '2>=1'
+assert 1 '0>=0'
+assert 0 '2>=3'
+
 echo OK
